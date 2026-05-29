b5eb132183a51ea60d84dda6f52383e7973e3376
EugenePrygaro <eugeneprygaro@gmail.com>
2026-05-29 15:28:51 +0300

    fixes

--- src/server.js
+++ src/server.js
@@ -8,18 +8,16 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
 
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
+app.use(cors());
 const app = express();
 app.use(
   express.json({
     limit: '100kb',
   }),
 );
-const PORT = process.env.PORT ?? 3000;
-
-// Глобальні middleware
-app.use(logger);
-app.use(cors());
-app.use(express.json());
 
 app.use(notesRoutes);
 
