65295f81889bd41517a1e5483eeaca65a2db5011
EugenePrygaro <eugeneprygaro@gmail.com>
2026-05-29 15:16:47 +0300

    init

---
+++ src/controllers/notesController.js
@@ -0,0 +1,47 @@
+import { Notes } from '../models/note.js';
+import createHttpError from 'http-errors';
+
+export const getNotes = async (req, res) => {
+  const notes = await Notes.find();
+  res.status(200).json(notes);
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Notes.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Notes.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Notes.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Notes.findOneAndUpdate({ _id: noteId }, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+  res.status(200).json(note);
+};
