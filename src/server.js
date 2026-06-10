68d34ec70e2fcc356fa972c87864e344680627a8
EugenePrygaro <eugeneprygaro@gmail.com>
2026-06-10 14:14:33 +0300

    tasks init

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -22,6 +23,7 @@ app.use(
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 
 app.use(errorHandler);
 
